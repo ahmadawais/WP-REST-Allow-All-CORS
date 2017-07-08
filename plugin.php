a6cb91a7f7bf63caf4f838074ef57629f03a6841
Ahmad Awais <mrahmadawais@gmail.com>
2017-07-08 05:25:34 +0500

    ADD: PHPCS Ruleset 💯

--- plugin.php
+++ plugin.php
@@ -17,26 +17,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Make sure the function is unique.
-if ( ! function_exists( 'wp_rest_allow_all_cors' ) ) {
-	// Hook.
-	add_action( 'rest_api_init', 'wp_rest_allow_all_cors', 15 );
+// Hook.
+add_action( 'rest_api_init', 'wp_rest_allow_all_cors', 15 );
 
-	/**
-	 * Allow all CORS.
-	 *
-	 * @since 1.0.0
-	 */
-	function wp_rest_allow_all_cors() {
-		// Remove the default filter.
-		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+/**
+ * Allow all CORS.
+ *
+ * @since 1.0.0
+ */
+function wp_rest_allow_all_cors() {
+	// Remove the default filter.
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
 
-		// Add a Custom filter.
-		add_filter( 'rest_pre_serve_request', function( $value ) {
-			header( 'Access-Control-Allow-Origin: *' );
-			header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
-			header( 'Access-Control-Allow-Credentials: true' );
-			return $value;
-		});
-	} // End fucntion wp_rest_allow_all_cors().
-} // End if().
+	// Add a Custom filter.
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
+		return $value;
+	});
+} // End fucntion wp_rest_allow_all_cors().
